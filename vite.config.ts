8e2eb0d25145752dd98606c1532bea7f64cda3bb
mnaserio121-cmyk <mnaserio121@gmail.com>
2025-11-26 09:36:01 +0330

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '');
+  // Casting process to any to avoid TS errors during build
+  const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
     plugins: [react()],
     define: {
-      // Passes the API_KEY from the build environment (Vercel) to the browser
+      // Passes the API_KEY from the build environment (Netlify/Vercel) to the browser
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
   }
